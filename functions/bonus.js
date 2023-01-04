ff435a1e158b5118c00cbaec6ec5b7f5341301a1
Dendeey <davidsim1035@gmail.com>
2023-01-04 16:26:33 +0000

    Functions

--- functions/bonus.js
+++ functions/bonus.js
@@ -5,12 +5,11 @@ function sortNumbers()
     let numbers = [-23,42,21,90,54,362,-12,57,78,84,35,546,128,7,15,65,28];
     for(let i = 0 ; i < numbers.length ; i++)
     {
-        console.log(numbers[i]);
+        let minNumber = i;
+        for(let j = i + 1; j < numbers.length; j++)
+        {
+            if(numbers[j] < numbers[minNumber])
+        }
     }
-    
-    
-    
+
 }
-
-let result = sortNumbers();
-
