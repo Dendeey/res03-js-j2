f2f0623d4c198f0a05b4323f39332f19e1e1494e
Dendeey <davidsim1035@gmail.com>
2023-01-04 14:32:00 +0000

    Functions

---
+++ functions/calculatrice.js
@@ -0,0 +1,73 @@
+/* Exo 1 */
+
+function add(number1, number2)
+{
+    return number1 + number2;
+}
+
+let result = add(2, 3);
+console.log(result);
+
+function sub(number1, number2)
+{
+    return number1 - number2;
+}
+
+let result1 = sub(2, 3);
+console.log(result1);
+
+function mult(number1, number2)
+{
+    return number1 * number2;
+}
+
+let result2 = mult(2, 3);
+console.log(result2);
+
+function div(number1, number2)
+{
+    return number1 / number2;
+}
+
+let result3 = div(2, 3);
+console.log(result3);
+
+function mod(number1, number2)
+{
+    return number1 % number2;
+}
+
+let result4 = mod(2, 3);
+console.log(result4);
+
+/* Exo 2 */
+
+function askUserOperation()
+{
+    let UserOperation = window.prompt("Quelle opération souhaitez-vous faire ?");
+    return UserOperation;
+    
+}
+
+function askFirstNumber()
+{
+    let FirstNumber = window.prompt("Premier nombre ?");
+    return FirstNumber;
+    
+}
+
+function askSecondNumber()
+{
+    let SecondNumber = window.prompt("Second nombre ?");
+    return SecondNumber;
+}
+
+
+/* Exo 3 */
+
+function calculator() 
+{
+    
+}
+
+calculator();
